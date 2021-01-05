cdc65aff9e64b7bd52c5bbd59a2af5c2d725f336
yauhofung <yauho.fung@student.mec.cuny.edu>
2021-01-05 18:48:47 -0500

    Updated code.js

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
 	MEC TTP 2021 Winter Assignment 2: Blackjack Game Engine
@@ -7,70 +7,87 @@
 	- To practice implementing core javascript principles within the framing of building a blackjack card game engine.
 	
 	===== MAJOR FEATURES =====
-	- User should be able to start the Blackjack game.
 	- User should be able to hit.
 	- User should be able to stand.
 	- User should be able to check status.
 	- User should be able to reset the game.
+	- User should be able to start the Blackjack game.
 
 	===== FUNCTION STUBS =====
-	- start()
 	- hit()
 	- stand()
 	- status()
 	- reset()
+	- start()
 
 	===== GOTCHAS =====
 	- The same number cannot be dealt more than four times.
 
-	===== REDUCE THE PROBLEM SPACE =====
-	- For now, assume duplicates are allowed.
+	===== PROBLEM SPACE =====
+	- For now, assume the same number can be dealt more than four times.
 */
 
-// let diamond_suit = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
-// let heart_suit = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
-// let spades_suit = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
-// let clubs_suit = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+// takes a random element from deck and pushes it into userHand
+function hit() {
 
-// There should be a random function that selects a random card from a random deck.
-
-let deck = [
-	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
-	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
-	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
-	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10
-];
-
-let wins = 0;
-let losses = 0;
-// let ties = 0;
-
-let user_hands = [];
-let ai_hands = [];
-
-//var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
-
-function totalSum(arr) {
-	let sum = 0;
-	for (let index = 0; index < arr.length; index++) {
-		sum += arr[index];
-	}
-	return sum;
 }
 
+// compares total card values and updates wins/losses/ties
+function stand() {
+
+}
+
+// prints status of all hands
+function status() {
+
+}
+
+// returns the game to the initial state
+function reset() {
+	var deck = [
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10
+	];
+
+	var wins = 0;
+	var losses = 0;
+	var ties = 0;
+
+	var aiHand = [];
+	var userHand = [];
+}
+
+// starts the Blackjack game
+function start() {
+	reset();
+}
+
+
+// ===== BRAINSTORM SECTION =====
+
+// // returns the total card value of hand
+// function calculateTotal(hand) {
+// 	let sum = 0;
+// 	for (let index = 0; index < hand.length; index++) {
+// 		sum += hand[index];
+// 	}
+// 	return sum;
+// }
+
 /*
-//
 while(userSum < 22 && aiSum < 22 && endGame = 1){
-  let endGame = 0;
+  var endGame = 0;
   function stand(){
 	endGame = 1;
   }
 
   function hit(){
-  user_hands.push(deck[Math.floor(Math.random()*deck.length)])
+  userHand.push(deck[Math.floor(Math.random()*deck.length)])
   }
   function aiHit(){
-	ai_hands.push(deck[Math.floor(Math.random()*deck.length)])
+	aiHand.push(deck[Math.floor(Math.random()*deck.length)])
   }
   function ifOver17(aiHand){
 	if(aiHand > 17){
@@ -80,42 +97,39 @@ while(userSum < 22 && aiSum < 22 && endGame = 1){
 }
 */
 
-function dealCards() {
-	user_hands = [];
-	ai_hands = [];
-	for (let index = 0; index < 2; index++) {
-		// todo: make sure that the two player's hands are of equal length
-		//add 2 cards to user and ai hands
-		user_hands.push(deck[Math.floor(Math.random() * deck.length)])
-		ai_hands.push(deck[Math.floor(Math.random() * deck.length)])
-	}
-	let userSum = totalSum(user_hands);
-	console.log(userSum);
-	let aiSum = totalSum(ai_hands);
-	console.log(aiSum);
+// function dealCards() {
+// 	userHand = [];
+// 	aiHand = [];
+// 	for (var index = 0; index < 2; index++) {
+// 		// todo: make sure that the two player's hands are of equal length
+// 		//add 2 cards to user and ai hands
+// 		userHand.push(deck[Math.floor(Math.random() * deck.length)])
+// 		aiHand.push(deck[Math.floor(Math.random() * deck.length)])
+// 	}
+// 	var userSum = calculateTotal(userHand);
+// 	console.log(userSum);
+// 	var aiSum = calculateTotal(aiHand);
+// 	console.log(aiSum);
 
-	//if aisum > 17 call ifOver17
-}
+// 	//if aisum > 17 call ifOver17
+// }
 //Can optimize code with class
 //Find out how to make a class with javascript
-
-
-dealCards();
 
 /*
 	if(Math.random() < .5){
 		// give to the player
 		if(Math.random() < .5){
-		user_hands.push(deck[index]);
+		userHand.push(deck[index]);
 		}else{
-		user_hands.unshift(deck[index]);
+		userHand.unshift(deck[index]);
 		}
 	}else{
 		// give to the ai
 		if(Math.random() < .5){
-		ai_hands.push(deck[index]);
+		aiHand.push(deck[index]);
 		}else{
-		ai_hands.unshift(deck[index]);
+		aiHand.unshift(deck[index]);
 		}
 	}
 
@@ -124,8 +138,8 @@ dealCards();
 
 function play_one_hand(){
 	return {
-	user_card: user_hands.pop(),
-	ai_card: ai_hands.pop()
+	user_card: userHand.pop(),
+	ai_card: aiHand.pop()
 	}
 }
 
@@ -142,7 +156,7 @@ function determine_outcome(hand){
 
 function start(){
 	deal_out_cards();
-	let hand = play_one_hand();
+	var hand = play_one_hand();
 	console.log(hand);
 	determine_outcome(hand);
 }
