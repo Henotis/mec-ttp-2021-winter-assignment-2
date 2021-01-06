4772bc02602da5b43fc4a7e1535bd294c56e4867
Henotis <Henryli@Mec.science>
2021-01-06 00:34:52 -0500

    re-removed some comments

--- code.js
+++ code.js
@@ -18,18 +18,8 @@
 	- reset()
 	- start()
 
-	===== GOTCHAS =====
-	- The same number cannot be dealt more than four times.
-
-	===== PROBLEM SPACE =====
-	- Assume there is no betting involved, only wins, losses, and ties.
-	- Assume there are only 2 players: the user and the dealer AI.
-	- Assume aces always count as 1.
-	- Assume there is no option to split, double down, or buy insurance.
-	- Assume that both the dealer's cards are given face up (ie visible).
 */
 
-// takes a random element from deck and pushes it into han
 let deck = [
 	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
 	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10,
@@ -85,7 +75,7 @@ function hit() {
 	cardRemove(index)
 	///////////////////////////
 	userTotal = userHand.reduce(reducer)
-	console.log(userTotal);
+	console.log("Your current Total: " + userTotal);
 	if (userTotal > 21) {
 		losses += 1;
 		console.log("You bust! Reset the game to play again!")
@@ -144,102 +134,3 @@ function start() {
 	userTotal = userHand.reduce(reducer)
 	console.log("User current hand: " + userTotal)
 }
-
-
-// ===== BRAINSTORM SECTION =====
-
-// // returns the total card value of hand
-// function calculateTotal(hand) {
-// 	let sum = 0;
-// 	for (let index = 0; index < hand.length; index++) {
-// 		sum += hand[index];
-// 	}
-// 	return sum;
-// }
-
-/*
-while(userSum < 22 && aiSum < 22 && endGame = 1){
-  let endGame = 0;
-  function stand(){
-	endGame = 1;
-  }
-
-  function hit(){
-  userHand.push(deck[Math.floor(Math.random()*deck.length)])
-  }
-  function aiHit(){
-	aiHand.push(deck[Math.floor(Math.random()*deck.length)])
-  }
-  function ifOver17(aiHand){
-	if(aiHand > 17){
-	  aiHit();
-	}
-  }
-}
-*/
-
-// function dealCards() {
-// 	userHand = [];
-// 	aiHand = [];
-// 	for (let index = 0; index < 2; index++) {
-// 		// todo: make sure that the two player's hands are of equal length
-// 		//add 2 cards to user and ai hands
-// 		userHand.push(deck[Math.floor(Math.random() * deck.length)])
-// 		aiHand.push(deck[Math.floor(Math.random() * deck.length)])
-// 	}
-// 	let userSum = calculateTotal(userHand);
-// 	console.log(userSum);
-// 	let aiSum = calculateTotal(aiHand);
-// 	console.log(aiSum);
-
-// 	//if aisum > 17 call ifOver17
-// }
-//Can optimize code with class
-//Find out how to make a class with javascript
-
-/*
-	if(Math.random() < .5){
-		// give to the player
-		if(Math.random() < .5){
-		userHand.push(deck[index]);
-		}else{
-		userHand.unshift(deck[index]);
-		}
-	}else{
-		// give to the ai
-		if(Math.random() < .5){
-		aiHand.push(deck[index]);
-		}else{
-		aiHand.unshift(deck[index]);
-		}
-	}
-
-	}
-}
-
-function play_one_hand(){
-	return {
-	user_card: userHand.pop(),
-	ai_card: aiHand.pop()
-	}
-}
-
-function determine_outcome(hand){
-	if(hand.user_card > hand.ai_card){
-	console.log('you won!');
-	wins++;
-	}else{
-	console.log('sorry, you lost!');
-	losses++;
-	}
-}
-
-
-function start(){
-	deal_out_cards();
-	let hand = play_one_hand();
-	console.log(hand);
-	determine_outcome(hand);
-}
-
-*/
